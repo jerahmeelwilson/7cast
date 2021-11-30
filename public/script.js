c3a283acb6cd53b17e2ec6a17bd57001172c3bdb
jerahmeelwilson <wilsonjerahmeel@gmail.com>
2021-11-30 13:10:59 -0600

    Added weather icons

--- public/script.js
+++ public/script.js
@@ -3,6 +3,7 @@ const citySearch = document.querySelector("[data-city-search]");
 const searchBox = new google.maps.places.SearchBox(citySearch);
 const cityCards = document.getElementById("cityCards");
 const modal_bg = document.querySelector(".modal-bg");
+
 class City {
   constructor(place_id, name, latitude, longitude, photo) {
     this.place_id = place_id;
@@ -46,18 +47,31 @@ function getCities() {
         .then((onecallData) => {
           console.log(onecallData.data);
           let currentWeather = onecallData.data.current.weather[0].main;
+          let currentWeatherDesc =
+            onecallData.data.current.weather[0].description;
           let temp_min = Math.round(onecallData.data.daily[0].temp.min);
           let current_temp = Math.round(onecallData.data.current.temp);
           let temp_max = Math.round(onecallData.data.daily[0].temp.max);
           let place_id = city.place_id;
+          let current_dt = onecallData.data.current.dt;
+          let current_sunset = onecallData.data.current.sunset;
           let cityCard = `
           <div class='city-card'>
-            <img class = 'card-img' src = ${city.photo} width ="250" height = "250">
+            <img class = 'card-img' src = ${
+              city.photo
+            } width ="325" height = "325">
             <div class = 'card-title'>
-              <h5> ${city.name}</h5> <span>${currentWeather}</span>
+              <h5> ${
+                city.name
+              }</h5> <div class ="card_current_weather"><span>${currentWeather}</span> ${getIcon(
+            currentWeather,
+            current_dt,
+            current_sunset,
+            currentWeather
+          )}</div>
             </div>
             <div class = 'card-temp'>
-              <p>Low: ${temp_min}°</p><p class='main-temp'>${current_temp}°</p><p>High: ${temp_max}°</p>
+              <p class ='card_low_temp'>Low: ${temp_min}°</p><p class='card_main-temp'>${current_temp}°</p><p class ="card_high_temp">High: ${temp_max}°</p>
             </div>
             <div class = 'card-bottom'>
               <button class="seemore ${place_id}">See more</button>
@@ -119,6 +133,48 @@ function deleteCity(e) {
 function dtConvert(dt) {
   let date = new Date(dt * 1000);
   return `${date.getMonth(date) + 1}/${date.getDate(date) + 1} `;
+}
+
+const weatherIcons = {
+  clear_sky_day: `<img src="images/amcharts_weather_icons_1.0.0/animated/day.svg" alt="">`,
+  clear_sky_night: `<img src="images/amcharts_weather_icons_1.0.0/animated/night.svg" alt="">`,
+  cloudy_day: `<img src="images/amcharts_weather_icons_1.0.0/animated/cloudy_day.svg" alt="">`,
+  cloudy_night: `<img src="images/amcharts_weather_icons_1.0.0/animated/cloudy_day.svg" alt="">`,
+  cloudy: `<img src="images/amcharts_weather_icons_1.0.0/animated/cloudy.svg" alt="">`,
+  drizzle: `<img src="images/amcharts_weather_icons_1.0.0/animated/rainy-1.svg" alt="">`,
+  rain: `<img src="images/amcharts_weather_icons_1.0.0/animated/rainy-6.svg" alt="">`,
+  snow: `<img src="images/amcharts_weather_icons_1.0.0/animated/snowy-5.svg" alt="">`,
+  thunder: `<img src="images/amcharts_weather_icons_1.0.0/animated/thunder.svg" alt="">`,
+};
+
+function getIcon(weather, current_time, sunset, description) {
+  if (weather == "Clear" && current_time < sunset) {
+    return weatherIcons.clear_sky_day;
+  } else if (weather == "Clear" && current_time > sunset) {
+    return weatherIcons.clear_sky_night;
+  } else if (
+    weather == "Clouds" &&
+    description == "few clouds" &&
+    current_time < sunset
+  ) {
+    return weatherIcons.cloudy_day;
+  } else if (
+    weather == "Clouds" &&
+    description == "few clouds" &&
+    current_time > sunset
+  ) {
+    return weatherIcons.cloudy_night;
+  } else if (weather == "Drizzle") {
+    return weatherIcons.drizzle;
+  } else if (weather == "Rain") {
+    return weatherIcons.rain;
+  } else if (weather == "Thunderstorm") {
+    return weatherIcons.thunder;
+  } else if (weather == "Snow") {
+    return weatherIcons.snow;
+  } else {
+    return weatherIcons.cloudy;
+  }
 }
 
 getCities();
